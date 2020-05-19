13591ccdb48c128fc829d3ed9b0aa96a56074652
Kiril Trpevski <kiril.trpevski@gmail.com>
2020-05-19 21:13:56 +0200

    first commit 19.05.2020

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { HotDealsComponent } from './hot-deals/hot-deals.component';
+import { PhoneComponent } from './phone/phone.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'hot-deals', component: HotDealsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'phones', component: PhoneComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatSliderModule} from '@angular/material/slider';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuHeaderComponent } from './menu-header/menu-header.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { HotDealsComponent } from './hot-deals/hot-deals.component';
+import { PhoneItemComponent } from './phone/phone-item/phone-item.component';
+import { PhoneComponent } from './phone/phone.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuHeaderComponent,
+    HomeComponent,
+    ContactComponent,
+    HotDealsComponent,
+    PhoneItemComponent,
+    PhoneComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatSliderModule,
+        BrowserAnimationsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
