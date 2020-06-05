fe9b91c088fc59f022969d7e598701df43998fdf
Kiril Trpevski <kiril.trpevski@gmail.com>
2020-06-05 17:00:09 +0200

    05.06.2020 (last checks)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB498in47z5dGnye3EyD_D7GOmlg7wiZbI',
+    authDomain: 'mobilestore-2df05.firebaseapp.com',
+    databaseURL: 'https://mobilestore-2df05.firebaseio.com',
+    projectId: 'mobilestore-2df05',
+    storageBucket: 'mobilestore-2df05.appspot.com',
+    messagingSenderId: '227802862575',
+    appId: '1:227802862575:web:c9ab5a364f30ff93b79c1a'
+  }
 };
